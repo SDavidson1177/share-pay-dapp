45c69374b0de4d9910f6f837f035f8776f7c4c86
SDavidson1177 <SDavidson1177@gmail.com>
2025-01-30 09:43:10 -0500

    able to list bills

--- app/src/components/owner/Bill.tsx
+++ app/src/components/owner/Bill.tsx
@@ -3,12 +3,31 @@ import { useState, useEffect, useRef} from 'react'
 import { Denominations, DenomMultiplier} from '../shared/constants';
 import { ethers } from 'ethers'
 import { FormUnit } from '../shared/form';
+import { Result } from 'ethers';
+import { weiToEther } from '../../utils/operations';
+
+interface Bill {
+    title: string
+    amount: bigint
+}
+
+function BillList({bills} : {bills: Bill[] | undefined}) {
+    useEffect(() => {
+        console.log(bills)
+    })
+
+    return (<>{bills?.map(v =>
+            <h4>{v.title} | {weiToEther(v.amount)} ETH</h4>)}
+    </>)
+}
 
 export function BillPanel({contract, signer} : {contract: ethers.Contract | undefined, signer: ethers.JsonRpcSigner | undefined}) {
     const {
         register,
         handleSubmit,
       } = useForm();
+
+    const [bills, setBills] = useState<Bill[]>()
 
     const create_bill = async (data: any) => {
         alert(JSON.stringify(data))
@@ -21,7 +40,23 @@ export function BillPanel({contract, signer} : {contract: ethers.Contract | unde
 
         let resp = await contract?.getBill(signer?.address, data.name)
         console.log(resp)
-    } 
+    }
+
+    const list_bills = async () => {
+        let resp = await contract?.getBills(signer?.address)
+        let resp_arr = resp.toArray()
+        let bill_arr: Bill[] = []
+        resp_arr.forEach((v: Result) => {
+            console.log(v.toObject())
+            let v_obj = v.toObject()
+            bill_arr.push({
+                title: v_obj.title,
+                amount: v_obj.amount,
+            })
+        })
+        console.log(bill_arr)
+        setBills(bill_arr)
+    }
 
     return (<>
             <h3>Create Bill</h3>
@@ -37,5 +72,10 @@ export function BillPanel({contract, signer} : {contract: ethers.Contract | unde
                 <FormUnit register={register}></FormUnit><br></br>
                 <button type='submit' >Create</button>
             </form>
+            <h3>Active Bills:</h3>
+            <BillList bills={bills}/>
+            <button style={{ padding: 10, margin: 10 }} onClick={list_bills}>
+                List Bills
+            </button><br></br>
         </>)
 }
